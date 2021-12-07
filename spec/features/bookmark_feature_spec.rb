6835cc7eea9cc2b25056a96e487dfd6c2b7c9970
zidehen <zoeidehen@gmail.com>
2021-12-07 14:23:11 +0000

    updated views.erb

--- spec/features/bookmark_feature_spec.rb
+++ spec/features/bookmark_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'Viewing bookmarks' do
     visit('/bookmarks')
 
     expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.skysports.com"
     expect(page).to have_content "http://www.google.com"
   end
 end
