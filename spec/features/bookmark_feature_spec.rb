7af7d46563d7cec1637fb1426981a17bcddee57d
Bryn <bryn2009@live.co.uk>
2021-12-07 15:35:48 +0000

    connected the DB

--- spec/features/bookmark_feature_spec.rb
+++ spec/features/bookmark_feature_spec.rb
@@ -8,8 +8,8 @@ feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
     visit('/bookmarks')
 
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.skysports.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.makersacademy.com/"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content "http://www.google.com/"
   end
 end
